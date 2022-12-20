7f5cbd2117a947709532a2b9836de5e8c3874cfb
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-12-20 20:17:57 +0100

    configs

--- src/test/scala/ecdsa/EcdsaSpec.scala
+++ src/test/scala/ecdsa/EcdsaSpec.scala
@@ -9,12 +9,11 @@ import scala.util.Random
 object EcdsaSpec extends ZIOSpecDefault {
   override def spec: Spec[TestEnvironment with Scope, Any] = suite("")(
     test("") {
-      val secp256k1CurveConfig = EllipticCurveConfig(0, 7, BigInt("115792089237316195423570985008687907853269984665640564039457584007908834671663"))
 
-      val x = BigInt("55066263022277343669578718895168534326250603453777594175500187360389116729240")
-      val y = BigInt("32670510020758816978083085130507043184471273380659243275938904335757337482424")
-      val n = BigInt("115792089237316195423570985008687907852837564279074904382605163141518161494337")
-      val g_point = EllipticCurvePoint(x, y, secp256k1CurveConfig)
+      val x = Curve.x
+      val y = Curve.y
+      val n = Curve.n
+      val g_point = Curve.g_point
 
       def signMessage(message: BigInt, privateKey: BigInt): (BigInt, BigInt) = {
         val k = BigInt(n.bitLength, new Random())
